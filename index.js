33e6b21f9f269bee837d8d66ca216e4fe4333fdc
Priscila Lima <94015991+prilrosas@users.noreply.github.com>
2024-04-17 14:59:41 +0000

    endpoint post e put

--- index.js
+++ index.js
@@ -12,11 +12,60 @@ app.get('/oi', function (req, res) {
 
   // Lista de itens
 
-  const itens = ['Mulher-Maravilha', 'Capitão-America', 'Hulk', 'Thor']
+  const lista = ['Mulher-Maravilha', 'Capitão-America', 'Hulk', 'Thor']
   
   //Endpoint Read All -> [get] /item
   app.get('/item', function(req, res) {
-    res.send(itens)
+    res.send(lista)
+  })
+
+  // Endpoint Read By ID [GET] /item/:id
+  app.get('/item/:id' , function (req,res){
+
+    // Acessamos o parametro de rota id
+    const id = req.params.id
+
+    //acessamos o item na lista (usando o id - 1)
+    // e colocamos na variavel item
+    const item = lista[id - 1]
+
+    // Enviamos para a resposta o item acessado
+    res.send(item)
+  })
+
+  //Especificamos que o corpo da requisição sera em JSON
+
+  app.use(express.json())
+
+  //Endpoint Create [POST] /item
+  app.post('/item', function(req, res) {
+
+    // pegamos o item atraves do corpo da requisição
+    // no objeto JSON, pegamos a propriedade nome
+    const item = req.body.nome
+
+    //adicionamos o item obtido na lista
+    lista.push(item)
+
+    // exibimos a uma mensagem de sucesso
+    res.send('Item adicionado com sucesso: ' + item)
+  })
+
+  //Endpoint Update -> [PUT] /item/:id
+
+  app.put('/item/:id', function(req, res) {
+
+    //Otemos o ID do parametro de rota
+    const id = req.params.id
+
+    // Obtemos o corpo da requisição para saber qual novo valor 
+    const novoItem = req.body.nome
+
+    // Atualizamos o item na lista
+    lista[id -1] = novoItem
+
+    // Enviamos uma mensagem de sucesso
+    res.send('Item atualizado com sucesso: ' +id + '.' +novoItem)
   })
 
 app.listen(3000)
