50847c293d77c3f6535d1fa500b3d45504eaa3b5
Priscila Lima <94015991+prilrosas@users.noreply.github.com>
2024-04-17 13:27:38 +0000

    criado endpoint get

--- index.js
+++ index.js
@@ -9,4 +9,14 @@ app.get('/oi', function (req, res) {
     res.send('ola mundo')
   })
 
+
+  // Lista de itens
+
+  const itens = ['Mulher-Maravilha', 'Capitão-America', 'Hulk', 'Thor']
+  
+  //Endpoint Read All -> [get] /item
+  app.get('/item', function(req, res) {
+    res.send(itens)
+  })
+
 app.listen(3000)
